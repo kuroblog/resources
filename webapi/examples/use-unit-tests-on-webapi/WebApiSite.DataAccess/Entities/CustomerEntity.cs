42498ce0e2ed6a1a1191bed688d257b5a5ca4bae
kuro <kuro_q@live.com>
2017-11-22 21:06:37 +0800

    add bll and dal projects

---
+++ webapi/examples/use-unit-tests-on-webapi/WebApiSite.DataAccess/Entities/CustomerEntity.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WebApiSite.DataAccess.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CustomerEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
